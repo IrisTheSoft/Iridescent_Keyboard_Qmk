a4eff7cd9f49cf2c927d99268eb75ea08ac16a17
Iridiscencia Álvarez <airidiscencia4@proton.me>
2025-03-30 10:51:22 -0300

    Configured underglow

--- config.h
+++ config.h
@@ -20,9 +20,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#define MASTER_RIGHT
+
 #define QUICK_TAP_TERM 0
 #define TAPPING_TERM 100
 
+/*
 #undef RGBLIGHT_LED_COUNT
 #define RGBLIGHT_EFFECT_BREATHING
 #define RGBLIGHT_EFFECT_RAINBOW_MOOD
@@ -39,10 +42,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
+*/
 
 // Underglow
-/*
 #undef RGBLIGHT_LED_COUNT
 #define RGBLIGHT_LED_COUNT 14    // Number of LEDs
 #define RGBLIGHT_SLEEP
-*/
+#define RGBLIGHT_SPLIT
+#define RGBLIGHT_DEFAULT_HUE 234 // 256*11/12
+#define RGBLIGHT_DEFAULT_SAT 128
+#define RGBLIGHT_DEFAULT_VAL 64
